2886d78c7952ea52e0d45cf4c240f431a4f46444
piumal1999 <piumalrathnayake@hotmail.com>
2023-06-11 11:57:35 +0530

    Create subscribe component

--- code/frontend/src/pages/Home.tsx
+++ code/frontend/src/pages/Home.tsx
@@ -2,31 +2,42 @@ import { Card, Col, Row } from "antd";
 import Title from "antd/es/typography/Title";
 import { FC } from "react";
 import Map from "../components/Map";
+import SubscribeForm from "../components/SubscribeForm";
 
 const Home: FC = () => {
   return (
     <div>
       <Title>Real Time Flood Forecast</Title>
-
-      {/* ant design 2 column card layout */}
       <Row gutter={16}>
-        <Col span={6}>
-          <Card title="In flow Level" bordered={false}>
-            <Title style={{ marginTop: 0 }}> 123 </Title>
-          </Card>
-        </Col>
-        <Col span={6}>
-          <Card title="Flood status" bordered={false}>
-            <Title style={{ marginTop: 0 }} type="danger">
-              {" "}
-              Flood{" "}
-            </Title>
-          </Card>
+        <Col span={12}>
+          <Row gutter={16} style={{ marginBottom: "3rem" }}>
+            <Col span={12}>
+              <Card title="Stream Flow" bordered={false}>
+                <Title style={{ marginTop: 0 }}> 0.255 m³/s </Title>
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card title="Flood status" bordered={false}>
+                <Title style={{ marginTop: 0 }} type="danger">
+                  Flood
+                </Title>
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={24}>
+              <Card title="Get Flood Alerts" bordered={false}>
+                <SubscribeForm />
+              </Card>
+            </Col>
+          </Row>
         </Col>
         <Col span={12}>
-          <Card title="Card title" bordered={false}>
-            <Map />
-          </Card>
+          <Col span={24}>
+            <Card title="Real time map" bordered={false}>
+              <Map />
+            </Card>
+          </Col>
         </Col>
       </Row>
     </div>
